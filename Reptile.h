6a09eea7db9fdb9e75fb71ab74a29e98b9f4805e
mostafaelsheehy8 <mostafa.elsheehy@student.giu-uni.de>
2025-11-12 15:13:20 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -3,7 +3,20 @@
 
 #include "Animal.h"
 
-//define reptile class here
+#include<iostream>
+using namespace std;
+
+class Reptile:public Animal{
+    
+    private:
+    bool isvenomous;
+    public:
+    Reptile(string n,int a,bool is,bool isv);
+    Reptile();
+    ~Reptile();
+    
+    
+};
 
 
 
